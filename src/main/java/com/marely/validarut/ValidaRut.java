ae6ac9df514adfeb848c5a6f644e632ba04eed99
marely <marely.carcamo@gmail.com>
2024-01-26 05:19:34 -0300

    comentarios

--- src/main/java/com/marely/validarut/ValidaRut.java
+++ src/main/java/com/marely/validarut/ValidaRut.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
  *
  * @author marely
  */
+/**
+ * The ValidaRut class in Java prompts the user to enter a Chilean national identification number (RUT)
+ * and validates whether it is a valid RUT or not.
+ */
 public class ValidaRut {
 
     public static void main(String[] args) {
@@ -29,6 +33,12 @@ public class ValidaRut {
     /*
     Este método valida el dato ingresado, verificando si corresponde al formato establecido, para posteriormente realizar el proceso de validación.
      */
+   /**
+    * The function "validarDato" checks if a given string matches a specific pattern.
+    * 
+    * @param dato The parameter "dato" is a string that represents a data value.
+    * @return The method is returning a boolean value.
+    */
     public static boolean validarDato(String dato) {
         return dato.matches("^\\d{1,8}-[\\dKk]$");
     }
@@ -37,6 +47,8 @@ public class ValidaRut {
     El método valida el rut chileno
     se engresa como argumento el rut y el digito verificado y retorna un valor booleano
      */
+   // The `validarRut` method is responsible for validating the Chilean national identification number
+   // (RUT).
     public static boolean validarRut(String texto, String dvRut) {
         int pos = texto.length();
         int suma = 0;
